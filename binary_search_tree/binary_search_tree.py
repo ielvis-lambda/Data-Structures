1406fd31644a458069aa45a87920b6622d607ff9
Elvis Ibarra <ielvisd@gmail.com>
2019-02-27 15:03:06 -0800

    insert complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,10 @@
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
@@ -5,8 +12,27 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    current = self
+    complete = False
+    while not complete:
+      # If the value is less than the current value
+      if value < current.value:
+        # add value to the left node
+        if current.left:
+          current = current.left
+        else:
+          current.left = Node(value)
+          complete = True
+      # If the value is more than the current value
+      if value > current.value:
+        # add value to the right node
+        if current.right:
+          current = current.right
+        else:
+          current.right = Node(value)
+          complete = True
 
+# push test
   def contains(self, target):
     pass
 
