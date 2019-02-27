73479dba04fdaafd0906e481174ab043f5960a96
Elvis Ibarra <ielvisd@gmail.com>
2019-02-27 15:18:20 -0800

    binary search tree complete

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,6 +5,7 @@ class Node:
         self.right = None
         self.left = None
 
+
 class BinarySearchTree:
   def __init__(self, value):
     self.value = value
@@ -32,9 +33,30 @@ class BinarySearchTree:
           current.right = Node(value)
           complete = True
 
-# push test
   def contains(self, target):
-    pass
+        # set a reference to self
+        current = self
+        while True:
+          # Will return False if doesn't contain the item
+            if not current:
+                return False
+          # If there's a match return True
+            if current.value == target:
+                return True
+          # If it's bigger check a node to the left which is lower than current otherwise check right
+            elif current.value > target:
+                current = current.left
+            else:
+                current = current.right
 
   def get_max(self):
-    pass
+      # Set a reference to self
+        current = self
+        max = 0
+        while current:
+          # if the current value is greater than the max set it to the current value and move right to check again
+            if current.value > max:
+                max = current.value
+                current = current.right
+
+        return max
